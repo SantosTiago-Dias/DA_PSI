129af578a0a77f17ed1be30ec3638a371bdf2c59
Tiago Santos <tiago.m.santos.201323@gmail.com>
2022-05-25 18:44:45 +0100

    Restaurante falta verificar o codgio postal

--- Inicial/Restaurante_From.Designer.cs
+++ Inicial/Restaurante_From.Designer.cs
@@ -31,16 +31,21 @@
             this.txtCidade = new System.Windows.Forms.TextBox();
             this.txtRua = new System.Windows.Forms.TextBox();
             this.txtCodPostal = new System.Windows.Forms.TextBox();
-            this.btnAddMorada = new System.Windows.Forms.Button();
+            this.btnaddRestaurante = new System.Windows.Forms.Button();
             this.dgRestaurante = new System.Windows.Forms.DataGridView();
-            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.cidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.morada = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cod_Postal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnEliminar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgRestaurante)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,6 +55,7 @@
             this.txtCidade.Name = "txtCidade";
             this.txtCidade.Size = new System.Drawing.Size(100, 20);
             this.txtCidade.TabIndex = 0;
+            this.txtCidade.TextChanged += new System.EventHandler(this.txtCidade_TextChanged);
             // 
             // txtRua
             // 
@@ -65,39 +71,30 @@
             this.txtCodPostal.Size = new System.Drawing.Size(100, 20);
             this.txtCodPostal.TabIndex = 2;
             // 
-            // btnAddMorada
+            // btnaddRestaurante
             // 
-            this.btnAddMorada.Location = new System.Drawing.Point(537, 221);
-            this.btnAddMorada.Name = "btnAddMorada";
-            this.btnAddMorada.Size = new System.Drawing.Size(162, 23);
-            this.btnAddMorada.TabIndex = 3;
-            this.btnAddMorada.Text = "Adicionar Restaurante";
-            this.btnAddMorada.UseVisualStyleBackColor = true;
-            this.btnAddMorada.Click += new System.EventHandler(this.btnAddMorada_Click);
+            this.btnaddRestaurante.Location = new System.Drawing.Point(537, 213);
+            this.btnaddRestaurante.Name = "btnaddRestaurante";
+            this.btnaddRestaurante.Size = new System.Drawing.Size(162, 23);
+            this.btnaddRestaurante.TabIndex = 3;
+            this.btnaddRestaurante.Text = "Adicionar Restaurante";
+            this.btnaddRestaurante.UseVisualStyleBackColor = true;
+            this.btnaddRestaurante.Click += new System.EventHandler(this.btnaddRestaurante_Click_1);
             // 
             // dgRestaurante
             // 
             this.dgRestaurante.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgRestaurante.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
             this.nome,
             this.cidade,
-            this.morada});
-            this.dgRestaurante.Location = new System.Drawing.Point(25, 50);
+            this.morada,
+            this.Cod_Postal});
+            this.dgRestaurante.Location = new System.Drawing.Point(12, 38);
             this.dgRestaurante.Name = "dgRestaurante";
-            this.dgRestaurante.Size = new System.Drawing.Size(388, 150);
+            this.dgRestaurante.Size = new System.Drawing.Size(494, 206);
             this.dgRestaurante.TabIndex = 4;
             this.dgRestaurante.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgRestaurante_CellContentClick);
-            // 
-            // nome
-            // 
-            this.nome.HeaderText = "Nome Restaurante";
-            this.nome.Name = "nome";
-            // 
-            // cidade
-            // 
-            this.cidade.HeaderText = "Cidade";
-            this.cidade.Name = "cidade";
-            this.cidade.ReadOnly = true;
             // 
             // label1
             // 
@@ -146,29 +143,90 @@
             this.txtNome.Size = new System.Drawing.Size(100, 20);
             this.txtNome.TabIndex = 8;
             // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(537, 276);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(162, 23);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(599, 12);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 11;
+            this.txtId.Visible = false;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "Id";
+            this.ID.Name = "ID";
+            this.ID.Visible = false;
+            // 
+            // nome
+            // 
+            this.nome.HeaderText = "Nome Restaurante";
+            this.nome.Name = "nome";
+            this.nome.ReadOnly = true;
+            this.nome.Width = 150;
+            // 
+            // cidade
+            // 
+            this.cidade.HeaderText = "Cidade";
+            this.cidade.Name = "cidade";
+            this.cidade.ReadOnly = true;
+            this.cidade.Width = 150;
+            // 
             // morada
             // 
             this.morada.HeaderText = "Morada";
             this.morada.Name = "morada";
             this.morada.ReadOnly = true;
+            this.morada.Width = 150;
+            // 
+            // Cod_Postal
+            // 
+            this.Cod_Postal.HeaderText = "Codigo Postal";
+            this.Cod_Postal.Name = "Cod_Postal";
+            this.Cod_Postal.ReadOnly = true;
+            this.Cod_Postal.Visible = false;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(537, 242);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(162, 23);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "Eliminar Restaurante";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // Restaurante_From
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(812, 311);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtNome);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dgRestaurante);
-            this.Controls.Add(this.btnAddMorada);
+            this.Controls.Add(this.btnaddRestaurante);
             this.Controls.Add(this.txtCodPostal);
             this.Controls.Add(this.txtRua);
             this.Controls.Add(this.txtCidade);
             this.Name = "Restaurante_From";
             this.Text = "Restaurante_From";
+            this.Load += new System.EventHandler(this.Restaurante_From_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgRestaurante)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -180,15 +238,20 @@
         private System.Windows.Forms.TextBox txtCidade;
         private System.Windows.Forms.TextBox txtRua;
         private System.Windows.Forms.TextBox txtCodPostal;
-        private System.Windows.Forms.Button btnAddMorada;
+        private System.Windows.Forms.Button btnaddRestaurante;
         private System.Windows.Forms.DataGridView dgRestaurante;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
         private System.Windows.Forms.DataGridViewTextBoxColumn nome;
         private System.Windows.Forms.DataGridViewTextBoxColumn cidade;
         private System.Windows.Forms.DataGridViewTextBoxColumn morada;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cod_Postal;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
--- Inicial/Restaurante_From.cs
+++ Inicial/Restaurante_From.cs
@@ -20,65 +20,140 @@ namespace Inicial
             ler_dados();
         }
 
-        private void btnAddMorada_Click(object sender, EventArgs e)
-        {
-            Morada morada = new Morada();
-            
-            morada.Rua = txtRua.Text;
-            morada.Cidade = txtCidade.Text;
-            morada.Cod_Postal = txtCodPostal.Text;
-
-            restGest.Morada.Add(morada);
-            restGest.SaveChanges();
-            Restaurante restaurante = new Restaurante();
-            restaurante.Nome = txtNome.Text;
-            restaurante.MoradaId = morada.Id;
-
-            restGest.Restaurante.Add(restaurante);
-            restGest.SaveChanges();
-
-        }
+       
 
         public void ler_dados()
         {
+            dgRestaurante.Rows.Clear();
+            dgRestaurante.Refresh();
             int i = 0;
             // query para dar join das duas tabelas e as juntar os campos de ambas
-            var innerJoinQuery =(
+            var query =(
                      from Restaurante in restGest.Restaurante
                      join Morada in restGest.Morada on Restaurante.MoradaId equals Morada.Id
-                     select new { Nome = Restaurante.Nome, Cidade = Morada.Cidade, Rua = Morada.Rua }).ToList();
-
-            MessageBox.Show(innerJoinQuery.Count().ToString());
-            Array arr = innerJoinQuery.ToArray();
-            foreach (var res in innerJoinQuery)
+                     select new { id=Restaurante.Id, Nome = Restaurante.Nome, Cidade = Morada.Cidade, Rua = Morada.Rua, Cod_Postal= Morada.Cod_Postal }).ToList();
+            //foreach para percorrer os campos do query
+            foreach (var res in query)
             {
                 dgRestaurante.Rows.Add();
+                dgRestaurante.Rows[i].Cells["ID"].Value = res.id;
                 dgRestaurante.Rows[i].Cells["nome"].Value = res.Nome;
                 dgRestaurante.Rows[i].Cells["cidade"].Value = res.Cidade;
                 dgRestaurante.Rows[i].Cells["morada"].Value = res.Rua;
-               
+                dgRestaurante.Rows[i].Cells["Cod_Postal"].Value = res.Cod_Postal;
                 i++;
             }
-            // for para percorrer as linahs da query
-            /*for (int i = 0; i <= innerJoinQuery.Count(); i++)
-            {
-                //foreach para carregar cada resultado da query
-                foreach (var res in innerJoinQuery)
-                {
-                    dgRestaurante.Rows[i].Cells["nome"].Value = res.Nome[i];
-                    dgRestaurante.Rows[i].Cells["cidade"].Value = res.Cidade[i];
-                }
-                
-
-            }*/
+            
         }
 
         
         private void dgRestaurante_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+           
+            limpar_txt();
+            btnaddRestaurante.Text = "Editar Restaurante";
+            int row = dgRestaurante.CurrentRow.Index;
+            txtId.Text = dgRestaurante.Rows[row].Cells["ID"].Value.ToString();
+            txtNome.Text = dgRestaurante.Rows[row].Cells["nome"].Value.ToString();
+            txtCidade.Text = dgRestaurante.Rows[row].Cells["cidade"].Value.ToString();
+            txtRua.Text = dgRestaurante.Rows[row].Cells["morada"].Value.ToString();
+            txtCodPostal.Text = dgRestaurante.Rows[row].Cells["Cod_Postal"].Value.ToString();
+
+            
+        }
+
+        private void btnaddRestaurante_Click_1(object sender, EventArgs e)
+        {
+            if (txtNome.Text != "" && txtRua.Text != "" && txtCodPostal.Text != "" && txtCidade.Text != "")
+            {
+                if(txtId.Text=="")
+                {
+                    Morada morada = new Morada();
+
+                    morada.Rua = txtRua.Text;
+                    morada.Cidade = txtCidade.Text;
+                    morada.Cod_Postal = txtCodPostal.Text;
+
+                    restGest.Morada.Add(morada);
+                    restGest.SaveChanges();
+                    Restaurante restaurante = new Restaurante();
+                    restaurante.Nome = txtNome.Text;
+                    restaurante.MoradaId = morada.Id;
+
+                    restGest.Restaurante.Add(restaurante);
+                    restGest.SaveChanges();
+                    
+                }
+                else
+                {
+                    int id = Convert.ToInt32(txtId.Text);
+                    Restaurante restaurante = restGest.Restaurante.Find(id);
+                    Morada morada = restGest.Morada.Find(restaurante.MoradaId);
+
+                    restaurante.Nome = txtNome.Text;
+                    morada.Rua = txtRua.Text;
+                    morada.Cidade = txtCidade.Text;
+                    morada.Cod_Postal = txtCodPostal.Text;
+                    restGest.SaveChanges();
+                }
+
+                
+            }
+            else
+            {
+                MessageBox.Show("Tem de preencher os campos todos", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            ler_dados();
+        }
+
+        public void limpar_txt()
+        {
+            txtId.Text = "";
+            txtCidade.Text = "";
+            txtCodPostal.Text = "";
+            txtNome.Text = "";
+            txtRua.Text = "";
+            
+        }
+
+        private void txtCidade_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Restaurante_From_Load(object sender, EventArgs e)
+        {
 
         }
 
-        
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            limpar_txt();
+            btnaddRestaurante.Text = "Adicionar Restaurante";
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (txtId.Text != "") 
+            {
+                DialogResult resposta = MessageBox.Show("Deseja apagar os dados?", "Pergunta", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+                if (resposta == DialogResult.Yes)
+                {
+                    int id = Convert.ToInt32(txtId.Text);
+                    Restaurante restaurante = restGest.Restaurante.Find(id);
+                    Morada morada = restGest.Morada.Find(restaurante.MoradaId);
+
+                    restGest.Morada.Remove(morada);
+                    restGest.Restaurante.Remove(restaurante);
+                    restGest.SaveChanges();
+                    limpar_txt();
+                    ler_dados();
+                }
+            }
+            
+            
+        }
     }
 }
